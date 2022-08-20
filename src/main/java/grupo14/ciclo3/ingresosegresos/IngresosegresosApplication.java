3affd3179d1896837b66ab11ce5207d5f7e7427c
proxer1982 <juanc.zorro@gmail.com>
2022-08-20 11:49:42 -0500

    Subiendo spring boot

---
+++ src/main/java/grupo14/ciclo3/ingresosegresos/IngresosegresosApplication.java
@@ -0,0 +1,13 @@
+package grupo14.ciclo3.ingresosegresos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IngresosegresosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IngresosegresosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/grupo14/ciclo3/ingresosegresos/IngresosegresosApplicationTests.java
@@ -0,0 +1,13 @@
+package grupo14.ciclo3.ingresosegresos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IngresosegresosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
